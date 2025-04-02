8aa71f61077951e8d33aa63fd6435b0a7b760a8c
Jose <jose.estrada.24e@utzmg.edu.mx>
2025-04-02 14:51:17 -0600

    Actualización del proyecto

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "crud_vue_9qu3_user", // Tu usuario de Render
-    password: "1WQWFlTr1mRHBaJmX68n8kyfqko6iWgL", // Tu contraseña de Render
-    host: "dpg-cvgnf48gph6c73bmil10-a.ohio-postgres.render.com", // El host de Render
+    user: "vue_6ov9_user", // Tu usuario de Render
+    password: "DEgOdW3sP9p5tFu4Tjod9KtgNOQlyU3B", // Tu contraseña de Render
+    host: "dpg-cvmq29be5dus739megr0-a.oregon-postgres.render.com", // El host de Render
     port: 5432, // El puerto por defecto de PostgreSQL
-    database: "crud_vue_9qu3", // El nombre de la base de datos en Render
+    database: "vue_6ov9", // El nombre de la base de datos en Render
     ssl: { rejectUnauthorized: false } // Descomenta esto si necesitas usar SSL
 });
 
